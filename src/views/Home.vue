e7ec946cfda7738ea80828d3e6a4613700692f47
Mark-Kohrman <mark.kohrman@gmail.com>
2020-12-31 15:51:04 -0600

    get ticket date from rails, initial index/home setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+ 
+<div class="container">
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">Street No.</th>
+      <th scope="col">Street Direction</th>
+      <th scope="col">Street Name</th>
+    </tr>
+  </thead>
+  <tbody>
+  <div v-for="ticket in tickets">
+    <tr>
+      <td>{{ticket.date}}</td>
+      <td>{{ticket.street_no}}</td>
+      <td>{{ticket.direction}}</td>
+      <td>{{ticket.street_name}}</td>
+    </tr>
+  </div>
+  </tbody>
+   </table>
+    </div>
   </div>
 </template>
 
@@ -8,13 +31,21 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "(ur)in trouble",
+      tickets: [],
     };
   },
-  created: function () {},
+  created: function () {
+    axios.get("/api/tickets").then((response) => {
+      console.log("these are all the tickets", response);
+      this.tickets = response.data;
+    });
+  },
   methods: {},
 };
 </script>
