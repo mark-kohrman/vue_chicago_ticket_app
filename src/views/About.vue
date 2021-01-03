85b0e2db6b2a2ccacd02308c8d8ce8a77a0a2e4a
Mark-Kohrman <mark.kohrman@gmail.com>
2021-01-03 16:40:28 -0600

    redo table for compatibility with Vue2filters. Updated About Page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About (ur)in trouble</h1>
+    <p> Mark Kohrman created this app in late 2020 and early 2021 based on a job posting.  It used a Ruby on Rails backend to add all of the Chicago public urination tickets that were issued in a seeds file.  I then built a Vue frontend to display all the data, a Bootstrap table to organize it and make it look nice, and a Bootstrap theme as well.  I installed Vue2Filters so the user can search by street name to see how many tickets were issued in their area.  A very simple app!</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-    <div>  
-      <b-table hover :items="tickets" :fields="fields"></b-table>
-   </div>
+    <h1>{{ title }}</h1>
+    <input type="text" v-model="searchTerm">
 
+
+ <div class="container">
+  <div v-for="ticket in filterBy(tickets, searchTerm, 'street_name')">
+
+  <table class="table">
+    <thead>
+        <tr>
+          <th scope="col">Date</th>
+          <th scope="col">Street No</th>
+          <th scope="col">Direction</th>
+          <th scope="col">Street Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{ ticket.date  }}</td>
+          <td>{{ ticket.street_no }} </td>
+          <td>{{ ticket.direction  }} </td>
+          <td>{{ ticket.street_name  }}</td>
+        </tr>
+      </tbody>
+  </table>
   </div>
+  </div>
+</div>
 
 </template>
 
@@ -14,13 +36,15 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
   data: function () {
     return {
-      message: "(ur)in trouble!!!",
+      title: "(ur)in trouble",
       tickets: [],
       fields: ["date", "street_no", "direction", "street_name"],
+      searchTerm: "",
     };
   },
   created: function () {
@@ -30,5 +54,6 @@ export default {
     });
   },
   methods: {},
+  mixins: [Vue2Filters.mixin],
 };
 </script>
